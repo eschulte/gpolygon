ce282c3d4a0d424552c8dfed9d52defc8612dfb4
Eric Schulte <schulte.eric@gmail.com>
2013-07-02 23:07:49 -0600

    actually, this is no fun

--- gpolygon.lisp
+++ gpolygon.lisp
@@ -5,31 +5,6 @@
 
 (start (make-instance 'hunchentoot:easy-acceptor :port 4242))
 
-
-;; two simple functions from the tutorial
-(define-easy-handler (tutorial1 :uri "/tutorial1") ()
-  (with-html-output-to-string (s)
-    (:html
-     (:head (:title "tutorial 1 (gpolygon)"))
-     (:body (:h2 "example the first")
-            "please click on this thing"
-            :br
-            (:a :href "#" :onclick (ps (alert "pooped on you"))
-                "I am the thing to be clicked")))))
-
-(define-easy-handler (tutorial2 :uri "/tutorial2") ()
-  (with-html-output-to-string (s)
-    (:html
-     (:head (:title "tut 2"))
-     (:script :type "text/javascript"
-              (str (ps (defun greet-callback ()
-                         (alert "fart in your direction"))))))
-    (:body
-     (:h2 "paren tutorial the second")
-     "here's some more stuff on which to click"
-     (:a :href "#" :onclick (ps (greet-callback)) "me me me"))))
-
-
 ;;; Drawing functions
 (define-easy-handler (draw :uri "/draw") ()
   "Draw a simple rectangle."
@@ -37,12 +12,20 @@
     (:html
      (:head
       (:script :type "text/javascript"
-               (str (ps (defun draw ()
+               (str (ps (defun draw (points)
                           (let ((c2 (chain document
                                            (get-element-by-id "main")
-                                           (get-context "2d"))))
+                                           (get-context "2d")))
+                                (head (chain points pop)))
                             (setf (@ c2 fill-style) "#f00")
-                            (chain c2 (fill-rect 10 10 80 80))))))))
-     (:body :onload (ps (draw))
+                            (chain c2 (begin-path))
+                            (chain c2 (move-to (@ head 0) (@ head 1)))
+                            (chain points ;; a javascript map, not a CL map
+                                   (map (lambda (point)
+                                          (chain c2 (line-to (@ point 0)
+                                                             (@ point 1))))))
+                            (chain c2 (close-path))
+                            (chain c2 (fill))))))))
+     (:body :onload (ps (draw '((10 10) (25 10) (50 50) (10 25))))
             (:canvas :id "main" :width 100 :height 100
                      :style "border: 1px solid black;")))))
